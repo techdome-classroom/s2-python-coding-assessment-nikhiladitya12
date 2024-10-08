fb73b49048cdbab45ad40b31d830312f9d4b03ec
Nikhil Aditya <nikhiladitya.22@gmail.com>
2024-10-08 14:16:52 +0530

    Both problems solved

--- program2.py
+++ program2.py
@@ -4,7 +4,32 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        pass
+          roman_map = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+        
+        total = 0
+        n = len(s)
+        
+        for i in range(n):
+         
+            current_val = roman_map[s[i]]
+            next_val = roman_map[s[i + 1]] if i < n - 1 else 0
+            
+            if current_val < next_val:
+               
+                total -= current_val
+            else:
+                
+                total += current_val
+        
+        return total
 
 
 
